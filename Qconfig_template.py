f65e604c9dfc921b47035de42497cdd027ae4099
quantumofme <46560500+quantumofme@users.noreply.github.com>
2020-11-17 17:55:25 +0100

    Delete Qconfig_template.py

--- Qconfig_template.py
+++
@@ -1,9 +0,0 @@
-APItoken = 'PUT_YOUR_API_TOKEN_HERE'
-
-config = {
-    'url': 'PUT_YOUR_URL_HERE', # For normal external users of IBMQX, the url is usually 
-                                # 'https://quantumexperience.ng.bluemix.net/api'
-    'hub': None,
-    'group': None,
-    'project': None
-}
